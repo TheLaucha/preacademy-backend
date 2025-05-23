756c7a7f08b129fb9b4ba6e264729f59dad45284
Lautaro <espinillo.lautaro@gmail.com>
2025-05-23 09:12:34 -0300

    Add Product, CartItem and Product classes

---
+++ src/main/primera_evaluacion/model/Coupon.java
@@ -0,0 +1,48 @@
+package main.primera_evaluacion.model;
+
+public class Coupon {
+    private String code;
+    private double discountPercentage;
+
+    public Coupon(){}
+
+    private Coupon(Builder builder){
+        this.code = builder.code;
+        this.discountPercentage = builder.discountPercentage;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public static class Builder{
+        private String code;
+        private double discountPercentage;
+
+        public Builder code(String code){
+            this.code = code;
+            return this;
+        }
+
+        public Builder discountPercentage(double discountPercentage){
+            this.discountPercentage = discountPercentage;
+            return this;
+        }
+
+        public Coupon build(){
+            return new Coupon(this);
+        }
+    }
+}
