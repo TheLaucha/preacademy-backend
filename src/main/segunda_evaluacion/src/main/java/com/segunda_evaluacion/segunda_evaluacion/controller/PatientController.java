ffcb5cb0c8a39c752fd5b5ab2e661d02faa9ca2b
Lautaro <espinillo.lautaro@gmail.com>
2025-08-02 20:17:39 -0300

    WIP: Empece a trabajar en los controllers pero prefiero crear los repositories y services primero

--- src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/controller/PatientController.java
+++ src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/controller/PatientController.java
@@ -1,5 +1,28 @@
 package com.segunda_evaluacion.segunda_evaluacion.controller;
 
+import com.segunda_evaluacion.segunda_evaluacion.model.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
 public class PatientController {
-  
+
+  @GetMapping
+  ResponseEntity<List<Patient>> getAllPatients(){
+    List<Patient> patients = List.of(new Patient(), new Patient(), new Patient());
+
+    return ResponseEntity.ok(patients);
+  }
+
+  @GetMapping("/{id}")
+  ResponseEntity<Patient> getPatientById(@PathVariable Long patientId){
+    Patient patient = new Patient();
+
+    return ResponseEntity.ok(patient);
+  }
 }
