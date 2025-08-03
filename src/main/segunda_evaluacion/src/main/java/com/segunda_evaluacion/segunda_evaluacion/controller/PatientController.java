468c5ddcfca24d29b29f2c35c803d51f2852b846
Lautaro <espinillo.lautaro@gmail.com>
2025-08-03 00:32:42 -0300

    feat: implemento el controller PatientController

--- src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/controller/PatientController.java
+++ src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/controller/PatientController.java
@@ -1,28 +1,62 @@
 package com.segunda_evaluacion.segunda_evaluacion.controller;
 
+import com.segunda_evaluacion.segunda_evaluacion.dto.PatientRequestDTO;
+import com.segunda_evaluacion.segunda_evaluacion.exception.RecursoNoEncontradoException;
 import com.segunda_evaluacion.segunda_evaluacion.model.Patient;
+import com.segunda_evaluacion.segunda_evaluacion.service.PatientService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
-@RequiredArgsConstructor
 public class PatientController {
+  private final PatientService patientService;
+
+  @Autowired
+  public PatientController(PatientService patientService){
+    this.patientService = patientService;
+  }
 
   @GetMapping
   ResponseEntity<List<Patient>> getAllPatients(){
-    List<Patient> patients = List.of(new Patient(), new Patient(), new Patient());
+    List<Patient> patients = patientService.findAll();
 
     return ResponseEntity.ok(patients);
   }
 
   @GetMapping("/{id}")
   ResponseEntity<Patient> getPatientById(@PathVariable Long patientId){
-    Patient patient = new Patient();
+    Patient patient =  patientService.getPatientById(patientId)
+        .orElseThrow(() -> new RecursoNoEncontradoException("No se encontro paciente con id: " + patientId));
 
     return ResponseEntity.ok(patient);
   }
+
+  @PostMapping
+  ResponseEntity<Patient> createPatient(@Valid @RequestBody PatientRequestDTO dto){
+    Patient patient = new Patient();
+    patient.setName(dto.getName());
+    patient.setLastname(dto.getLastname());
+    patient.setDni(dto.getDni());
+    patient.setEmail(dto.getEmail());
+
+    Patient savedPatient = patientService.savePatient(patient);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedPatient);
+  }
+
+  @DeleteMapping("/{id}")
+  ResponseEntity<Void> deletePatient(@PathVariable Long id){
+    patientService.deletePatient(id);
+
+    return ResponseEntity.noContent().build();
+  }
+
 }
--- src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/dto/PatientRequestDTO.java
+++ src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/dto/PatientRequestDTO.java
@@ -1,2 +1,25 @@
-package com.segunda_evaluacion.segunda_evaluacion.dto;public class PatientRequestDTO {
+package com.segunda_evaluacion.segunda_evaluacion.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PatientRequestDTO {
+  @NotBlank(message = "El nombre es obligatorio")
+  @Size(min = 3, max = 20, message = "El nombre debe tener entre 3 y 20 caracteres")
+  private String name;
+
+  @NotBlank(message = "El apellido es obligatorio")
+  @Size(min = 3, max = 20, message = "El apellido debe tener entre 3 y 20 caracteres")
+  private String lastname;
+
+  @NotBlank(message = "El dni es obligatorio")
+  @Size(min = 7, max = 10, message = "El DNI debe tener entre 7 y 10 caracteres")
+  private String dni;
+
+  @NotBlank(message = "El email es obligatorio")
+  @Email(message = "El formato de email no es valido")
+  private String email;
 }
