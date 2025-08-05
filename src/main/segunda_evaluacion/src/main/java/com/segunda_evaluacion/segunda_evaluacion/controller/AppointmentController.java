7609cf8c0e0f3430ee81e3ed269bf8113018d854
Lautaro <espinillo.lautaro@gmail.com>
2025-08-05 10:47:53 -0300

    feat: Añado AppointmentController endpoints GET /turnos y POST /turnos

--- src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/controller/AppointmentController.java
+++ src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/controller/AppointmentController.java
@@ -1,2 +1,63 @@
-package com.segunda_evaluacion.segunda_evaluacion.controller;public class AppointmentController {
+package com.segunda_evaluacion.segunda_evaluacion.controller;
+
+import com.segunda_evaluacion.segunda_evaluacion.dto.AppointmentRequestDTO;
+import com.segunda_evaluacion.segunda_evaluacion.model.Appointment;
+import com.segunda_evaluacion.segunda_evaluacion.model.Patient;
+import com.segunda_evaluacion.segunda_evaluacion.model.Professional;
+import com.segunda_evaluacion.segunda_evaluacion.service.AppointmentService;
+import com.segunda_evaluacion.segunda_evaluacion.service.PatientService;
+import com.segunda_evaluacion.segunda_evaluacion.service.ProfessionalService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/turnos")
+public class AppointmentController {
+  private final AppointmentService appointmentService;
+  private final PatientService patientService;
+  private final ProfessionalService professionalService;
+
+  @Autowired
+  public AppointmentController(AppointmentService appointmentService, PatientService patientService,
+                               ProfessionalService professionalService
+  ){
+    this.appointmentService = appointmentService;
+    this.patientService = patientService;
+    this.professionalService = professionalService;
+  }
+
+
+  @GetMapping
+  ResponseEntity<List<Appointment>> getAll(){
+    List<Appointment> appointments = appointmentService.findAll();
+
+    return ResponseEntity.ok(appointments);
+  }
+
+  @PostMapping
+  ResponseEntity<Appointment> createAppointment(@RequestBody @Valid AppointmentRequestDTO dto){
+
+    Optional<Patient> patient = patientService.getPatientById(dto.getPatientId());
+    Optional<Professional> professional = professionalService.findProfessionalById(dto.getProfessionalId());
+
+    if (patient.isEmpty() || professional.isEmpty()){
+      return ResponseEntity.notFound().build();
+    }
+
+    Appointment appointment = new Appointment();
+    appointment.setPatient(patient.get());
+    appointment.setProfessional(professional.get());
+    appointment.setDate(dto.getDate());
+
+    Appointment savedAppointment = appointmentService.createAppointment(appointment);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedAppointment);
+  }
+  
 }
--- src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/dto/AppointmentRequestDTO.java
+++ src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/dto/AppointmentRequestDTO.java
@@ -1,2 +1,23 @@
-package com.segunda_evaluacion.segunda_evaluacion.dto;public class AppointmentRequestDTO {
+package com.segunda_evaluacion.segunda_evaluacion.dto;
+
+import com.segunda_evaluacion.segunda_evaluacion.model.Patient;
+import com.segunda_evaluacion.segunda_evaluacion.model.Professional;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class AppointmentRequestDTO {
+  @NotNull(message = "El ID del paciente es obligatorio")
+  private Long patientId;
+
+  @NotNull(message = "El ID del profesional es obligatorio")
+  private Long professionalId;
+
+  @NotNull(message = "La fecha del turno es obligatoria")
+  private LocalDate date;
+
 }
