ffd4dfeb7119c420d498583e328a78919dc45bfc
Lautaro <espinillo.lautaro@gmail.com>
2025-08-05 09:10:45 -0300

    feat: implemento ProfessionalRequestDTO

---
+++ src/main/segunda_evaluacion/src/main/java/com/segunda_evaluacion/segunda_evaluacion/dto/ProfessionalRequestDTO.java
@@ -0,0 +1,18 @@
+package com.segunda_evaluacion.segunda_evaluacion.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ProfessionalRequestDTO {
+  @NotBlank(message = "El nombre completo es obligatorio")
+  @Size(min = 3, max = 100, message = "El campo fullname debe tener una longitud de entre 3 y 100 caracteres")
+  private String fullName;
+
+  @NotBlank(message = "La especialidad es obligatoria")
+  private String specialty;
+}
